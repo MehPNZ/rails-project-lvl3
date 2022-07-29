ec52f4c0a2594c7921cb714cd254d99307427e4f
Александр Михайлов <meh@MacBook-Air-Aleksandr.local>
2022-07-29 12:13:35 +0300

    deploy

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,6 +8,11 @@ import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
 
+import "bootstrap"
+import "../stylesheets/application"
+
+
 Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
+
