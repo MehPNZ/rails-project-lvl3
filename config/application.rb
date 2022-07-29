81fab512f31e92ffa0c6095ed4595744d6750329
Александр Михайлов <meh@MacBook-Air-Aleksandr.local>
2022-07-29 12:30:36 +0300

    deploy

--- config/application.rb
+++ config/application.rb
@@ -10,6 +10,9 @@ module BulletinBoard
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.1
+    
+    config.i18n.available_locales = %i[en ru]
+    config.i18n.default_locale = :ru
 
     # Configuration for the application, engines, and railties goes here.
     #
