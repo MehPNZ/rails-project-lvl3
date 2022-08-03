dcc8b52544517362898da5ec2b4aadffa119cfa6
Александр Михайлов <meh@MacBook-Air-Aleksandr.local>
2022-08-03 09:45:07 +0300

    deploy

--- app/controllers/web/admin_controller.rb
+++ app/controllers/web/admin_controller.rb
@@ -2,6 +2,6 @@ class Web::AdminController < ApplicationController
   before_action :set_bulletin, only: %i[ show edit update destroy ]
 
   def index
-    @pagy, @bulletins = pagy Bulletin.where(aasm_state: 'under_moderation').order(create_at: :desc)
+    @pagy, @bulletins = pagy Bulletin.where(aasm_state: 'under_moderation').order(created_at: :desc)
   end
 end
